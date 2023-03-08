f3e19f78581876d390a6c3af7ee52fd2ac28ac64
isa Muneretto <isamuneretto@gmail.com>
2023-03-08 14:30:52 -0300

    refactor: add route soma

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const express = require('express')
 const app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+//ROTA é a porta de entrada da nossa API, do nosso backend
+//Method HTTP - É a forma como a internet se comunica - CRUD (CREATE, READ, UPDATE, DELETE)
+    // GET - Pega uma info
+    // POST - Cria uma info
+    // PUT - Altera toda a info
+    // PATH - Altera parte da info
+    // DELETE - Apaga uma info
+
+//Name - É um identificador da rota
+
+//Function (Callback) - Responsavel por executar algum comando
+
+app.get('/soma', function (req, res) {
+    const soma = 1 + 1;
+    res.send({soma: soma})
+});
 
 app.listen(3000)
